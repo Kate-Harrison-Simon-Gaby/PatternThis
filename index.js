0739c19129d40298480e03c55988746175e71b7e
gabrielamelian <gabrielamelian@gmail.com>
2016-11-18 12:11:22 +1300

    Index.js functions setup done

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const reducer = require('./reducer')
 const template = require('./template')
 const generateBoard = require('./generateBoard')
 
-document.main.appendChild(mainView)
+document.main.appendChild(currentState)
 
-const intialState = generateBoard()
+const intialState = {
+    board: generateBoard()
+}
+
+const store = redux.createStore(reducer, intialState)
+const dispatch = store.dispatch
+
+const currentState = template(intialState.board, dispatch)
+
+const updateView = () => {
+    const initialState = store.getState()
+    const newState = template(intialState.board, dispatch)
+    morph(currentState, newState)
+}
+
+store.subscribe(updateView)
