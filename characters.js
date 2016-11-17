29534ff91671f2bed5241f483704c66830473654
Apprentice <apprentice@StMac0.local>
2016-11-18 12:38:49 +1300

    add characters functions

--- characters.js
+++ characters.js
@@ -1,30 +1,48 @@
+const clone = require('clone')
 
-const charArray = []
+
+var charArray = []
 
 function addChars(string){
   //take string
   //split into characters to be added (arr)
+  var chars = string.split('')
   //clone charArray
+  var arrCopy = clone(charArray)
   //pass chars to be added into charArray
+  chars.forEach(function(char){
     //add char where no duplicates
-  //return new charArray
+    if (!arrCopy.includes(char)) arrCopy.push(char)
+  })
+  charArray = arrCopy
+  return charArray
 }
 
 function removeChars(string){
   //take string
+  var chars = string.split('')
+  console.log("bork", chars);
   //split into characters to be added (arr)
   //clone charArray
+  var arrCopy = clone(charArray)
   //pass chars to be removed from charArray
-    //remove char where there is match
+     //remove char where there is match
+  var arr = arrCopy.filter(function(char){
+    if(!chars.includes(char)) return char
+  })
   //return new charArray
+  charArray = arr
+  return charArray
 }
 
 function outputChars(){
-  //return current charArray
+  return charArray
 }
 
-module.exports {
+module.exports = {
   addChars,
   removeChars,
   outputChars
 }
+console.log(addChars("abc"))
+console.log(removeChars("bc"))
